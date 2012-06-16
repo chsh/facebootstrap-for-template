aedaaa90371f0600cbad510cbd2519a495fb6e35
CHIKURA Shinsaku <shinsaku@chikura.me>
2012-06-17 02:44:03 +0900

    Add like a devise generated file.

---
+++ db/migrate/devise_create_users.rb
@@ -0,0 +1,14 @@
+class DeviseCreateUsers < ActiveRecord::Migration
+  def self.up
+    create_table(:users) do |t|
+      t.string :facebook_user_id, null: false
+      t.text :facebook_profile
+      t.timestamps
+    end
+    add_index :users, :facebook_user_id, unique: true
+  end
+
+  def self.down
+    drop_table :users
+  end
+end
